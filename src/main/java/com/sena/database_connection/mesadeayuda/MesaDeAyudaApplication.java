62911b46f236fd9cfa470e9b08b23a2ae5ad559b
valegiraldo846 <vbecerragiraldo@gmail.com>
2026-08-04 15:43:08 -0500

    first commit

---
+++ src/main/java/com/sena/database_connection/mesadeayuda/MesaDeAyudaApplication.java
@@ -0,0 +1,13 @@
+package com.sena.database_connection.mesadeayuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesaDeAyudaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MesaDeAyudaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sena/database_connection/mesadeayuda/MesaDeAyudaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.database_connection.mesadeayuda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesaDeAyudaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
