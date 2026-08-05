34a1bef98e60aacccb5e9d853048d2d454fe5e75
valegiraldo846 <vbecerragiraldo@gmail.com>
2026-08-05 07:21:23 -0500

    chore: configurar proyecto con MySQL y variables de entorno

--- src/main/java/entities/RefreshToken.java
+++ src/main/java/entities/RefreshToken.java
@@ -1,4 +1,13 @@
 package entities;
 
+import java.time.LocalDateTime;
+
 public class RefreshToken {
+    private long id;
+    private String token;
+    private Usuario usuario;
+    private LocalDateTime expiraEn;
+    private boolean revocado;
+
+
 }
